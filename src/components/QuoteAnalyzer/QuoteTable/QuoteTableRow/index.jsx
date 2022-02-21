d1071bf3c9d68b129847a9b7cf6fd725e1136af1
TJC <gamester4433@gmail.com>
2022-02-20 20:30:11 -0800

    Rendering basic table data

--- src/components/QuoteAnalyzer/QuoteTable/QuoteTableRow/index.jsx
+++ src/components/QuoteAnalyzer/QuoteTable/QuoteTableRow/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+function QuoteTableRowPrices(props) {
+
+}
+
 function QuoteTableRow(props) {
+  const { data } = props
+
   return (
     <tr>
-      <td>blah</td>
+      <td>{data['Location']}</td>
+      <td>{data['PartNo']}</td>
+      <td>{data['Product']}</td>
+      <td>{data['Weight'].toLocaleString("en-US")}</td>
+      { data['Quotes'].map((quote) => {
+        return <td>{quote['FinalPrice'].toLocaleString('en-US', {style:'currency', currency:'USD'})}</td>
+      })}
+      {/* <QuoteTableRowPrices /> */}
     </tr>
   )
 }
